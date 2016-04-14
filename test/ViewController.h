c5546d96363aaa6c979c1386a9a5cb070ce7824d
Sedat Balkan <sbalkan@Alper-Mac.local>
2016-04-14 05:04:43 -0700

    label and button implementation

--- test/ViewController.h
+++ test/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+@property (weak, nonatomic) IBOutlet UIButton *btnSave;
+- (IBAction)buttonClick:(id)sender;
 
 @end
 
